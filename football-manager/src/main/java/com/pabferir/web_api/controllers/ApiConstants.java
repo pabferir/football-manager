67026af56f57929eeb30b542ff93519cafb25106
paferran <pablo.ferrando-iranzo@capgemini.com>
2022-09-09 20:25:52 +0200

    refactor: Player Controllers path constant name

--- football-manager/src/main/java/com/pabferir/web_api/controllers/ApiConstants.java
+++ football-manager/src/main/java/com/pabferir/web_api/controllers/ApiConstants.java
@@ -7,5 +7,5 @@ public final class ApiConstants {
     public static final String API_V1 = API_BASE + "/v1";
 
     // API v1 Controller paths
-    public static final String CONTROLLER_PLAYERS = API_V1 + "/players";
+    public static final String PLAYER_CONTROLLER = API_V1 + "/player";
 }
