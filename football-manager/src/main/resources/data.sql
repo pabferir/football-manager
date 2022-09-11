549e310b5a33336bb379fedf6779cef8c7b3cf86
paferran <pablo.ferrando-iranzo@capgemini.com>
2022-09-12 00:42:30 +0200

    feature: added environment profiles

---
+++ football-manager/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO player (id, country_code, current_value_in_millions, date_of_birth, first_name, height, jersey_number, last_name, last_value_update, position_code)
+VALUES (1000,'FR',30,'1987-12-19','Karim',1.85,9,'Benzema','2022-06-03','FW');
+INSERT INTO player (id, country_code, current_value_in_millions, date_of_birth, first_name, height, jersey_number, last_name, last_value_update, position_code)
+VALUES (2000,'UY',4,'1987-02-14','Edinson',1.84,7,'Cavani','2022-06-15','FW');
+INSERT INTO player (id, country_code, current_value_in_millions, date_of_birth, first_name, height, jersey_number, last_name, last_value_update, position_code)
+VALUES (3000,'AR',50,'1987-06-24','Lionel',1.7,30,'Messi','2022-05-30','FW');
+INSERT INTO player (id, country_code, current_value_in_millions, date_of_birth, first_name, height, jersey_number, last_name, last_value_update, position_code)
+VALUES (4000,'FR',160,'1998-12-20','Kylian',1.78,7,'Mbappé','2022-05-30','FW');
+INSERT INTO player (id, country_code, current_value_in_millions, date_of_birth, first_name, height, jersey_number, last_name, last_value_update, position_code)
+VALUES (5000,'PT',30,'1985-02-05','Cristiano',1.87,7,'Ronaldo','2022-06-15','FW');
